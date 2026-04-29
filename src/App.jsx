a32308496cc37269537139a9b7b60a0d66cf593d
KIMJIHUN <kimjihun4118@gmail.com>
2026-04-29 17:08:49 +0900

    리액트 프로젝트 배포

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { Routes, Route } from 'react-router-dom';
+
+// 페이지 컴포넌트 import
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import ProjectDetail from './pages/ProjectDetail';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+
+// 레이아웃 컴포넌트 import
+import MainLayout from './layouts/MainLayout';
+
+function App() {
+  return (
+    <Routes>
+      {/* MainLayout을 부모로 두어 모든 페이지에 공통 레이아웃 적용 */}
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        
+        {/* 프로젝트 관련 라우팅 */}
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:id" element={<ProjectDetail />} />
+        
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+
+      {/* 레이아웃이 필요 없는 독립적인 페이지나 404 페이지 */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default App;
